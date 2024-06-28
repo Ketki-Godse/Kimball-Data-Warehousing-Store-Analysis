cb9635ca26f89e56b0dcccf0491b69523b6e84a1
Ketki-Godse <134458658+Ketki-Godse@users.noreply.github.com>
2024-06-27 18:41:11 -0700

    Add files via upload

---
+++ SQL queries/Dimension Tables Creation.sql
@@ -0,0 +1,109 @@
+﻿CREATE OR REPLACE TABLE Dim_Location(
+    DimLocationID INT IDENTITY(1,1) CONSTRAINT PK_DimLocationID PRIMARY KEY NOT NULL --Surrogate Key
+    ,Address VARCHAR(255)
+    ,City VARCHAR(255)
+    ,PostalCode VARCHAR(255) 
+    ,State_Province VARCHAR(255) 
+    ,Country VARCHAR(255) 
+);
+
+CREATE OR REPLACE TABLE Dim_Channel(
+    DimChannelID INT IDENTITY(1,1) CONSTRAINT PK_DimChannelID PRIMARY KEY NOT NULL --Surrogate Key
+    ,ChannelID INTEGER
+    ,ChannelCategoryID INTEGER
+    ,ChannelName VARCHAR(255) 
+    ,ChannelCategory VARCHAR(255)
+);
+
+CREATE OR REPLACE TABLE Dim_Customer(
+    DimCustomerID INT IDENTITY(1,1) CONSTRAINT PK_DimCustomerID PRIMARY KEY NOT NULL --Surrogate Key
+    ,CustomerID VARCHAR(16777216)
+    ,CustomerFullName VARCHAR(255)
+    ,CustomerFirstName VARCHAR(255) 
+    ,CustomerLastName VARCHAR(255)
+    ,CustomerGender VARCHAR(255)
+    ,DimLocationID INT NOT NULL, -- Assuming DimLocationID is an INT in Dim_Location
+    CONSTRAINT FK_DimCustomer_DimLocation FOREIGN KEY (DimLocationID)
+        REFERENCES Dim_Location(DimLocationID) -- Make sure Dim_Location and DimLocationID exist and are properly defined
+);
+
+CREATE OR REPLACE TABLE Dim_Store(
+     DimStoreID INT IDENTITY(1,1) CONSTRAINT DimStoreID PRIMARY KEY NOT NULL, 
+     DimLocationID INTEGER NOT NULL
+    ,SourceStoreID INTEGER NOT NULL --Natural Key
+    ,StoreName VARCHAR(255)
+    ,StoreNumber Number(38,0)
+    ,StoreManager VARCHAR(255)
+    ,CONSTRAINT DimLocationID FOREIGN KEY (DimLocationID) REFERENCES Dim_Location(DimLocationID) -- Foreign Key Constraint
+);
+
+CREATE OR REPLACE TABLE Dim_Reseller (
+    DimResellerID INT IDENTITY(1,1) CONSTRAINT DimStoreID PRIMARY KEY NOT NULL,
+    DimLocationID INTEGER NOT NULL,
+    ResellerID VARCHAR NOT NULL,
+    ResellerName VARCHAR(255),
+    ContactName VARCHAR(255),
+    PhoneNumber VARCHAR(255),
+    Email VARCHAR(255)
+    ,CONSTRAINT Dim_LocationID_ResellerID FOREIGN KEY (DimLocationID) REFERENCES Dim_Location(DimLocationID) -- Foreign Key Constraint
+);
+
+CREATE TABLE Dim_Product (
+    DimProductID INT IDENTITY(1,1) CONSTRAINT PK_DimProductID PRIMARY KEY NOT NULL, -- Surrogate Key
+    ProductID INTEGER,
+    ProductTypeID INTEGER,
+    ProductCategoryID INTEGER,
+    ProductName VARCHAR(255),
+    ProductType VARCHAR(255),
+    ProductCategory VARCHAR(255),
+    ProductRetailPrice FLOAT,
+    ProductWholesalePrice FLOAT,
+    ProductCost FLOAT,
+    ProductRetailProfit FLOAT AS (ProductRetailPrice - ProductCost) ,
+    ProductWholesaleUnitProfit FLOAT AS (ProductWholesalePrice - ProductCost) ,
+    ProductProfitMarginUnitPercent FLOAT AS 
+        CASE WHEN ProductRetailPrice <> 0 
+            THEN (ProductRetailPrice - ProductCost) / ProductRetailPrice * 100 
+            ELSE NULL 
+        END 
+);
+
+create or replace table DIM_DATE (
+	DATE_PKEY				number(9) PRIMARY KEY,
+	DATE					date not null,
+	FULL_DATE_DESC			varchar(64) not null,
+	DAY_NUM_IN_WEEK			number(1) not null,
+	DAY_NUM_IN_MONTH		number(2) not null,
+	DAY_NUM_IN_YEAR			number(3) not null,
+	DAY_NAME				varchar(10) not null,
+	DAY_ABBREV				varchar(3) not null,
+	WEEKDAY_IND				varchar(64) not null,
+	US_HOLIDAY_IND			varchar(64) not null,
+	/*<COMPANYNAME>*/_HOLIDAY_IND varchar(64) not null,
+	MONTH_END_IND			varchar(64) not null,
+	WEEK_BEGIN_DATE_NKEY		number(9) not null,
+	WEEK_BEGIN_DATE			date not null,
+	WEEK_END_DATE_NKEY		number(9) not null,
+	WEEK_END_DATE			date not null,
+	WEEK_NUM_IN_YEAR		number(9) not null,
+	MONTH_NAME				varchar(10) not null,
+	MONTH_ABBREV			varchar(3) not null,
+	MONTH_NUM_IN_YEAR		number(2) not null,
+	YEARMONTH				varchar(10) not null,
+	QUARTER					number(1) not null,
+	YEARQUARTER				varchar(10) not null,
+	YEAR					number(5) not null,
+	FISCAL_WEEK_NUM			number(2) not null,
+	FISCAL_MONTH_NUM		number(2) not null,
+	FISCAL_YEARMONTH		varchar(10) not null,
+	FISCAL_QUARTER			number(1) not null,
+	FISCAL_YEARQUARTER		varchar(10) not null,
+	FISCAL_HALFYEAR			number(1) not null,
+	FISCAL_YEAR				number(5) not null,
+	SQL_TIMESTAMP			timestamp_ntz,
+	CURRENT_ROW_IND			char(1) default 'Y',
+	EFFECTIVE_DATE			date default to_date(current_timestamp),
+	EXPIRATION_DATE			date default To_date('9999-12-31') 
+)
+comment = 'Type 0 Dimension Table Housing Calendar and Fiscal Year Date Attributes'; 
+
---
+++ SQL queries/Views.sql
@@ -0,0 +1,261 @@
+﻿--Question 1 View
+CREATE OR REPLACE SECURE VIEW question1 AS
+SELECT 
+    fs.DimStoreID,
+    ds.DATE_PKEY AS SaleDate,
+    dt.DATE_PKEY AS TargetDate,
+    ds.DAY_NAME as DAY_NAME,
+    ds.YEAR as YEAR,
+    ds.MONTH_NAME as MONTH_NAME,
+    ds._holiday_ind as ISHOLIDAY,
+    SUM(fs.SaleAmount) AS TotalSalesAmount,
+    SUM(fst.SalesTargetAmount) AS TotalSalesTarget,
+    SUM(fs.SaleTotalProfit) AS TotalProfit
+FROM
+    (Select DIMSTOREID,DIMRESELLERID,DIMCHANNELID,DIMSALEDATEID,count(*),SUM(SALEAMOUNT) as SaleAmount,
+    SUm(SALEQUANTITY) as SALEQUANTITY,
+    SUM(SALETOTALPROFIT) as SALETOTALPROFIT from Fact_SalesActual group by DIMSTOREID,DIMRESELLERID,DIMCHANNELID,DIMSALEDATEID) fs
+    JOIN Dim_Date ds ON fs.DimSaleDateID = ds.DATE_PKEY
+    LEFT JOIN Fact_SRCSalesTarget fst ON fs.DimStoreID = fst.DimStoreID
+        --AND fs.DimResellerID = fst.DimResellerID
+        --AND fs.DimChannelID = fst.DimChannelID
+        AND ds.DATE_PKEY = fst.DimTargetDateID
+    JOIN Dim_Date dt ON fst.DimTargetDateID = dt.DATE_PKEY
+WHERE
+    fs.DimStoreID IN (1, 5)
+GROUP BY
+    fs.DimStoreID,
+    ds.DATE_PKEY,
+    dt.DATE_PKEY,
+    ds.DAY_NAME,
+    ds.MONTH_NAME,
+    ds.YEAR,
+    ds._holiday_ind
+
+
+
+-- Question 2 View
+CREATE OR REPLACE SECURE VIEW question2 AS
+SELECT 
+    s.DimStoreID,
+    s.StoreNumber,
+    p.ProductType,
+    d.DATE_PKEY AS SaleYear,
+    d.DAY_NAME as DAY_NAME,
+    d.YEAR as YEAR,
+    d.MONTH_NAME as MONTH_NAME,
+    d._holiday_ind as ISHOLIDAY,
+    SUM(f.SaleAmount) AS TotalSales
+FROM 
+    Fact_SalesActual f
+JOIN 
+    Dim_Store s ON f.DimStoreID = s.DimStoreID
+JOIN 
+    Dim_Product p ON f.DimProductID = p.DimProductID
+JOIN 
+    Dim_Date d ON f.DimSaleDateID = d.DATE_PKEY
+WHERE 
+    p.ProductType IN ('Men\'s Casual', 'Women\'s Casual')
+    AND s.StoreNumber IN (5, 8)
+GROUP BY 
+    s.DimStoreID,
+    s.StoreNumber,
+    p.ProductType,
+    d.DATE_PKEY,
+    d.DAY_NAME,
+    d.MONTH_NAME,
+    d.YEAR,
+    d._holiday_ind
+;
+
+
+
+-- Question 3 View
+CREATE OR REPLACE SECURE VIEW question3 AS
+SELECT 
+    s.DimStoreID,
+    s.StoreNumber,
+    p.PRODUCTNAME,
+    p.ProductType,
+    d.DATE_PKEY AS SaleYear,
+    d.DAY_NAME as DAY_NAME,
+    d.YEAR as YEAR,
+    d.MONTH_NAME as MONTH_NAME,
+    d._holiday_ind as ISHOLIDAY,
+    SUM(f.SaleAmount) AS TotalSales,
+    SUM(f.SaleQuantity) AS TotalQuantity
+FROM 
+    Fact_SalesActual f
+JOIN 
+    Dim_Store s ON f.DimStoreID = s.DimStoreID
+JOIN 
+    Dim_Product p ON f.DimProductID = p.DimProductID
+JOIN 
+    Dim_Date d ON f.DimSaleDateID = d.DATE_PKEY
+WHERE 
+    s.StoreNumber IN (5, 8)
+GROUP BY 
+    s.DimStoreID,
+    s.StoreNumber,
+    p.productname,
+    p.ProductType,
+    d.DATE_PKEY,
+    d.DAY_NAME,
+    d.MONTH_NAME,
+    d.YEAR,
+    d._holiday_ind;
+
+
+
+
+-- Question 4 View
+CREATE OR REPLACE SECURE VIEW question4 AS
+SELECT
+    l.State_Province,
+    s.StoreNumber,
+    p.PRODUCTNAME,
+    p.ProductType,
+    d.DATE_PKEY AS SaleYear,
+    d.DAY_NAME as DAY_NAME,
+    d.YEAR as YEAR,
+    d.MONTH_NAME as MONTH_NAME,
+    (SELECT COUNT(DISTINCT s2.DimStoreID)
+     FROM Dim_Store s2
+     JOIN Dim_Location l2 ON s2.DimLocationID = l2.DimLocationID
+     WHERE l2.State_Province = l.State_Province) AS number_of_stores_in_state,
+    SUM(sa.SaleAmount) AS Total_Sales_Amount,
+    SUM(sa.SaleQuantity) AS Total_Sales_Quantity
+FROM
+    Fact_SalesActual sa
+JOIN
+    Dim_Store s ON sa.DimStoreID = s.DimStoreID
+JOIN
+    Dim_Location l ON s.DimLocationID = l.DimLocationID
+JOIN 
+    Dim_Product p ON sa.DimProductID = p.DimProductID
+JOIN
+    Dim_Date d ON sa.DimSaleDateID = d.Date_PKey
+GROUP BY
+    l.State_Province,
+    s.StoreNumber,
+    p.PRODUCTNAME,
+    p.ProductType,
+    d.DATE_PKEY,
+    d.DAY_NAME,
+    d.YEAR,
+    d.MONTH_NAME
+    ;
+
+
+// Standard views
+CREATE OR REPLACE SECURE VIEW View_Dim_Location AS
+SELECT
+    DimLocationID,
+    Address,
+    City,
+    PostalCode,
+    State_Province,
+    Country
+FROM
+    Dim_Location;
+
+CREATE OR REPLACE SECURE VIEW View_Dim_Customer AS
+SELECT
+    DimCustomerID,
+    CustomerID,
+    CustomerFullName,
+    CustomerFirstName,
+    CustomerLastName,
+    CustomerGender,
+    DimLocationID
+FROM
+    Dim_Customer;
+
+CREATE OR REPLACE SECURE VIEW View_Dim_Channel AS
+SELECT
+    DimChannelID,
+    ChannelID,
+    ChannelCategoryID,
+    ChannelName,
+    ChannelCategory
+FROM
+    Dim_Channel;
+
+CREATE OR REPLACE SECURE VIEW View_Dim_Store AS
+SELECT
+    DimStoreID,
+    DimLocationID,
+    SourceStoreID,
+    StoreNumber,
+    StoreManager
+FROM
+    Dim_Store;
+
+CREATE OR REPLACE SECURE VIEW View_Dim_Reseller AS
+SELECT
+    DimResellerID,
+    DimLocationID,
+    ResellerID,
+    ResellerName,
+    ContactName,
+    PhoneNumber,
+    Email
+FROM
+    Dim_Reseller;
+
+CREATE OR REPLACE SECURE VIEW View_Dim_Product AS
+SELECT
+    DimProductID,
+    ProductID,
+    ProductTypeID,
+    ProductCategoryID,
+    ProductName,
+    ProductType,
+    ProductCategory,
+    ProductRetailPrice,
+    ProductWholesalePrice,
+    ProductCost,
+    ProductRetailProfit,
+    ProductWholesaleUnitProfit,
+    ProductProfitMarginUnitPercent
+FROM
+    Dim_Product;
+
+CREATE OR REPLACE SECURE VIEW View_Fact_ProductSalesTarget AS
+SELECT
+    DimProductID,
+    DimTargetDateID,
+    ProductTargetSalesQuantity
+FROM
+    Fact_ProductSalesTarget;
+
+CREATE OR REPLACE SECURE VIEW View_Fact_SRCSalesTarget AS
+SELECT
+    DimStoreID,
+    DimResellerID,
+    DimChannelID,
+    DimTargetDateID,
+    SalesTargetAmount
+FROM
+    Fact_SRCSalesTarget;
+
+CREATE OR REPLACE SECURE VIEW View_Fact_SalesActual AS
+SELECT
+    DimProductID,
+    DimStoreID,
+    DimResellerID,
+    DimCustomerID,
+    DimChannelID,
+    DimSaleDateID,
+    DimLocationID,
+    SalesDetailID,
+    SalesHeaderID,
+    SaleAmount,
+    SaleQuantity,
+    SaleUnitPrice,
+    SaleExtendedCost,
+    SaleTotalProfit
+FROM
+    Fact_SalesActual;
+
+
